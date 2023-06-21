ad1cf02b6e6989fbaba2eb25998a93fb132b57ed
김영현 <agilecatch@gmail.com>
2023-06-21 17:32:09 +0900

    addfile

---
+++ lib/home_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'feed.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key}); //생성자
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white, //상단컬러
+        elevation: 0.5,
+        leading: Row //가로배치
+            (
+          children: [
+            SizedBox(width: 16),
+            Text(
+              '지산동',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: Colors.black,
+            ),
+          ],
+        ),
+        leadingWidth: 100, //크기지정
+        actions: [
+          // 오른쪽위 아이콘
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.search, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu_rounded, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.bell, color: Colors.black),
+          ),
+        ],
+      ),
+      body: feed(), //추출 코드분리
+      floatingActionButton: FloatingActionButton(
+        // 하단 플러스 버튼기능
+        onPressed: () {},
+        backgroundColor: Color(0xFFFF7E36),
+        elevation: 1,
+        child: Icon(
+          Icons.add_rounded,
+          size: 36,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        iconSize: 28,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: '홈',
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.my_library_books_outlined),
+            label: '동네생활',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fmd_good_outlined),
+            label: '내 근처',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chat_bubble_2),
+            label: '채팅',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: '나의 당근',
+          ),
+        ],
+        currentIndex: 0,
+      ),
+    );
+  }
+}
